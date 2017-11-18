9d02fb5396873705dc9349db3c0e7b47440d7818
Andie <andiedonovan@yahoo.com>
2017-11-17 23:59:36 -0800

    My Django Girls app, x commit

--- blog/models.py
+++ blog/models.py
@@ -3,21 +3,21 @@ from django.utils import timezone
 
 
 
-class Post(models.Model):
-	author = models.ForeignKey('auth.User') #blog post author
+class Post(models.Model): #creates/ defines an object named Post. models.Model indicates that its a Django Model
+	author = models.ForeignKey('auth.User') #blog post author; links to other model
 	title = models.CharField(max_length=200) #blog title; limits lenght
-	text = models.TextField() #blog body
+	text = models.TextField() #blog body; no limit
 	created_date = models.DateTimeField(
 		default=timezone.now)
 	published_date = models.DateTimeField(
 		blank=True, null=True)
 
 
-	def publish(self):
+	def publish(self): #publishing function
 		self.published_date = timezone.now()
 		self.save()
 
-	def __str__(self):
+	def __str__(self): #example of two dunders (double underscores)
 		return self.title
-		
+
 
